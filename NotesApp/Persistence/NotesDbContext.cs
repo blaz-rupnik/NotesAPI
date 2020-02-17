f88400d0ce4b701ac9943719114a046611c76fdb
Blaž <blaz.rupnik56@gmail.com>
2020-02-17 20:36:38 +0100

    Added db context and first migration to local db

---
+++ NotesApp/Persistence/NotesDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotesApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotesApp.Persistence
+{
+    public class NotesDbContext : DbContext
+    {
+        public NotesDbContext(DbContextOptions<NotesDbContext> options)
+            : base(options)
+        {
+            
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
